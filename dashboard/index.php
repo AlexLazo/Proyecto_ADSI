23836909d4e38cab1f3a3a169d65f2626883eb13
AlexLazo <alex.lazo1403@gmail.com>
2022-05-22 23:42:19 -0600

    first commit

---
+++ dashboard/index.php
@@ -0,0 +1,16 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Contenido principal</h1>
+    
+    
+    
+ <?php
+
+
+?>
+    
+</div>
+<!--FIN del cont principal-->
+<?php require_once "vistas/parte_inferior.php"?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+        
+        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
+        
+    </head>
+    
+    <body>
+     
+      <div class="container-login">
+          <div class="left">
+              <img src="img/03.png" alt="">
+          </div>
+      
+
+        <div class="wrap-login">
+            <form class="login-form validate-form" id="formLogin" action="" method="post">
+                <span class="login-form-title">Iniciar Sesión</span>
+                
+                <div class="wrap-input100" data-validate = "Usuario incorrecto">
+                    <input class="input100" type="text" id="usuario" name="usuario" placeholder="Usuario">
+                    <span class="focus-efecto"></span>
+                </div>
+                
+                <div class="wrap-input100" data-validate="Password incorrecto">
+                    <input class="input100" type="password" id="password" name="password" placeholder="Password">
+                    <span class="focus-efecto"></span>
+                </div>
+                
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">CONECTAR</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>     
+        
+        
+     <script src="jquery/jquery-3.3.1.min.js"></script>    
+     <script src="bootstrap/js/bootstrap.min.js"></script>    
+     <script src="popper/popper.min.js"></script>    
+        
+     <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+     <script src="js/conexion.js"></script>    
+    </body>
+</html>
